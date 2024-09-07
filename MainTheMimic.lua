68c94e6d45c460868a6227bc0e8ac3d969a96aa2
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-08 02:17:26 +0700

    Update MainTheMimic.lua

--- MainTheMimic.lua
+++ MainTheMimic.lua
@@ -548,15 +548,15 @@ if id == 7265396805 or id == 7251866503 then
                     {
                         Title = "Yes",
                         Callback = function()
-                                    TP.HumanoidRootPart.CFrame = CFrame.new(1099.39794921875, 3.135153293609619, 75.5241928100586)
-                                    wait(1.5)
-                            _G.buttlefly = true
-                            while _G.buttlefly do wait()
+                                TP.HumanoidRootPart.CFrame = CFrame.new(1099.39794921875, 3.135153293609619, 75.5241928100586)
+                                wait(1.5)
+                                _G.buttlefly = true
+                                while _G.buttlefly do wait()
                                 wait(0) -- Adjusted wait to avoid potential performance issues
                                 handleProximityPrompts()
                                 Autobtfs()
                                 fire()
-                            end
+                                end
                         end
                     },
                     {

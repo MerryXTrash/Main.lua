2ebea755337275166d66c687cac42062467a14ce
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-07 19:32:19 +0700

    Update MainTheMimic.lua

--- MainTheMimic.lua
+++ MainTheMimic.lua
@@ -1,18 +1,127 @@
+local TP = game.Players.LocalPlayer.Character
+local TweenService = game:GetService("TweenService")
+local id = game.PlaceId
+local RunService = game:GetService("RunService")
+
+local function Tween(Position, Duration)
+    local player = game.Players.LocalPlayer
+    local character = player.Character
+    local humanoidRootPart = character and character:FindFirstChild("HumanoidRootPart")
+    local humanoid = character and character:FindFirstChildOfClass("Humanoid")
+    
+    if humanoidRootPart then
+        if not syn then
+            if not humanoidRootPart:FindFirstChild("BodyVelocity1") then
+                if humanoid and humanoid.Sit then
+                    humanoid.Sit = false
+                end
+                
+                local bodyVelocity = Instance.new("BodyVelocity")
+                bodyVelocity.Name = "BodyVelocity1"
+                bodyVelocity.Parent = humanoidRootPart
+                bodyVelocity.Velocity = Vector3.new(0, 0, 0)
+                bodyVelocity.MaxForce = Vector3.new(100000, 100000, 100000)
+            end
+        end
+        
+        for _, part in pairs(character:GetDescendants()) do
+            if part:IsA("BasePart") then
+                part.CanCollide = false
+            end
+        end
+        
+        local tweenInfo = TweenInfo.new(
+            Duration,
+            Enum.EasingStyle.Linear,
+            Enum.EasingDirection.Out
+        )
+
+        local goal = {
+            CFrame = CFrame.new(Position)
+        }
+
+        local tween = TweenService:Create(humanoidRootPart, tweenInfo, goal)
+        tween:Play()
+
+        tween.Completed:Connect(function()
+            if humanoidRootPart:FindFirstChild("BodyVelocity1") then
+                humanoidRootPart.BodyVelocity1:Destroy()
+            end
+        end)
+    end
+end
+
+
+--local targetPosition = Vector3.new(10, 5, -10)
+--local duration = 3
+
+--Tween(targetPosition, duration)
+
+local TeleportService = game:GetService("TeleportService")
+        local Players = game:GetService("Players")
+        local LocalPlayer = Players.LocalPlayer
+        local newPlaceId = 7618863566
+                        
+        local function teleportToNewPlace()
+        local success, errorMessage = pcall(function()
+        TeleportService:Teleport(newPlaceId, LocalPlayer)
+        end)
+                        
+        if not success then
+        warn("Failed to teleport: " .. tostring(errorMessage))
+    end
+end
+
+local function Skip()
+if id == 6296321810 or id == 6479231833 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(3507.028564453125, 43.13663864135742, -1541.9735107421875) -- b1c1p1
+elseif id == 6301638949 or id == 6480994221 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(1272.7239990234375, 200.04153442382812, -2537.25) -- b1c1p2
+elseif id == 6373539583 or id == 6485055338 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(64.74767303466797, 60.94379806518555, -1622.5250244140625) -- b1c2p1
+elseif id == 6406571212 or id == 6485055836 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(232.6605987548828, 100.84130096435547, -595.3074951171875) -- b1c2p2
+elseif id == 6425178683 or id == 6485056556 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(1055.5179443359375, 78.26939392089844, -351.45831298828125) -- b1c2p3
+elseif id == 6472459099 or id == 6688734180 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(2416.63671875, -23.031118392944336, 2294.332275390625) -- b1c3p1
+elseif id == 6682163754 or id == 6688734313 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(241.43087768554688, 33.24515914916992, 450.21502685546875) -- b1c3p2
+elseif id == 6682164423 or id == 6688734395 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(-654.1986694335938, 648.9832153320312, -1012.5780029296875) -- b1c3p3
+elseif id == 7068738088 or id == 7068951438 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(2528.98681640625, 8.164044380187988, -1184.1917724609375) -- twt1
+elseif id == 7068951914 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(-444.71759033203125, 20.493558883666992, -1453.7325439453125) -- twt2
+elseif id == 7068740106 or id == 7068952294 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(2690.99169921875, 9.297460556030273, 649.3221435546875) -- twt3
+elseif id == 15996404472 or id == 15996407335 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(3507.028564453125, 43.13663864135742, -1541.9735107421875) -- b1c1p1
+elseif id == 15996410294 or id == 15996411979 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(64.74767303466797, 60.94379806518555, -1622.5250244140625) -- b1c2p1
+elseif id == 15996413469 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(1055.5179443359375, 78.26939392089844, -351.45831298828125) -- b1c2p3
+elseif id == 15996414738 or id == 15996416081 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(2411.138916015625, -23.03112030029297, 2267.944580078125) -- b1c3p1
+elseif id == 15996417416 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(431.4171142578125, 17.476003646850586, -846.1138916015625) -- b1c3p3
+elseif id == 7265396387 or id == 7251865082 then
+    TP.HumanoidRootPart.CFrame = CFrame.new(85.20524597167969, -51.00001525878906, -1415.0792236328125) -- b1c4p1
+end
+end
+
 _G.auto = false
 while _G.auto do wait()
 wait(0)
--- LocalScript ที่วางใน StarterPlayerScripts
 local player = game.Players.LocalPlayer
 local camera = workspace.CurrentCamera
 
--- ตั้งค่ามุมมองกล้องให้มองไปที่ด้านล่าง
 local function setCameraToLookDown()
     local cameraCFrame = camera.CFrame
-    local newLookVector = Vector3.new(0, -1, 0) -- มองไปที่ด้านล่าง
+    local newLookVector = Vector3.new(0, -1, 0)
     camera.CFrame = CFrame.new(cameraCFrame.Position, cameraCFrame.Position + newLookVector)
 end
 
--- เรียกใช้ฟังก์ชันเมื่อเริ่มเกม
 setCameraToLookDown()
 end
 
@@ -35,7 +144,6 @@ local function findProximityPrompt()
     return nil
 end
 
--- ฟังก์ชันในการเรียกใช้ ProximityPrompt
 local function triggerProximityPrompt(proximityPrompt, amount, skip)
     if proximityPrompt then
         local holdDuration = proximityPrompt.HoldDuration
@@ -70,7 +178,7 @@ function AutoOrbs()
         if v.Name == "Orb" then
             humanoidRootPart.CFrame = v.CFrame * CFrame.new(0, 5, 0)
             orbsFound = true
-            break  -- Exit the loop once an orb is found and the player is moved to it
+            break
         end
     end
     
@@ -80,20 +188,19 @@ function AutoOrbs()
     end
 end
 
--- ฟังก์ชันตรวจจับการเข้าใกล้
 local function checkProximity()
     local player = game.Players.LocalPlayer
     local character = player.Character or player.CharacterAdded:Wait()
     local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
     
     while true do
-        wait(0) -- ตรวจสอบทุกๆ 1 วินาที
+        wait(0)
         
-        local part = workspace:FindFirstChild("PartWithProximityPrompt") -- หรือใช้วิธีการค้นหาอื่น ๆ
+        local part = workspace:FindFirstChild("PartWithProximityPrompt")
         if part then
-            local distance = (humanoidRootPart.Position - part.Position).Magnitude -- ปรับระยะห่างตามที่ต้องการ
+            local distance = (humanoidRootPart.Position - part.Position).Magnitude
             
-            if distance < 10 then -- ปรับระยะห่างตามที่ต้องการ
+            if distance < 10 then
                 local proximityPrompt = findProximityPrompt()
                 triggerProximityPrompt(proximityPrompt, 1, false)
             end
@@ -101,7 +208,6 @@ local function checkProximity()
     end
 end
 
--- Create highlight folder and template
 local folder = Instance.new("Folder")
 folder.Name = "HighlightsFolder"
 folder.Parent = game.Workspace
@@ -113,13 +219,12 @@ highlightTemplate.FillTransparency = 0.5
 highlightTemplate.OutlineTransparency = 0
 highlightTemplate.Parent = folder
 
--- Functions for highlighting objects
 local function applyHighlight(instance)
     local highlight = highlightTemplate:Clone()
     highlight.Name = instance.Name .. "Highlight"
-    highlight.FillColor = Color3.fromRGB(255, 0, 0) -- Red color for highlighting
+    highlight.FillColor = Color3.fromRGB(255, 0, 0)
     highlight.FillTransparency = 0.8
-    highlight.OutlineColor = Color3.fromRGB(255, 255, 255) -- White outline
+    highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
     highlight.Parent = instance
 end
 
@@ -155,9 +260,6 @@ local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local Show = loadstring(game:HttpGet("https://raw.githubusercontent.com/MerryXTrash/Vscose/main/Toggle.lua"))()
 -- Initialize variables
-local TP = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-local TweenService = game:GetService("TweenService")
-local id = game.PlaceId
 
 local Window = Fluent:CreateWindow({
     Title = (id == 6296321810 or id == 6479231833 or id == 6301638949 or id == 6480994221) and "The Mimic | Book 1 Chapter 1" or
@@ -280,41 +382,7 @@ id == 15996413469 or id == 7265396387 or id == 7251865082 or id == then
                     {
                         Title = "Yes",
                         Callback = function()
-                            if id == 6296321810 or id == 6479231833 then
-                                TP = CFrame.new(3507.028564453125, 43.13663864135742, -1541.9735107421875) -- b1c1p1
-                            elseif id == 6301638949 or id == 6480994221 then
-                                TP = CFrame.new(1272.7239990234375, 200.04153442382812, -2537.25) -- b1c1p2
-                            elseif id == 6373539583 or id == 6485055338 then
-                                TP = CFrame.new(64.74767303466797, 60.94379806518555, -1622.5250244140625) -- b1c2p1
-                            elseif id == 6406571212 or id == 6485055836 then
-                                TP = CFrame.new(232.6605987548828, 100.84130096435547, -595.3074951171875) -- b1c2p2
-                            elseif id == 6425178683 or id == 6485056556 then
-                                TP = CFrame.new(1055.5179443359375, 78.26939392089844, -351.45831298828125) -- b1c2p3
-                            elseif id == 6472459099 or id == 6688734180 then
-                                TP = CFrame.new(2416.63671875, -23.031118392944336, 2294.332275390625) -- b1c3p1
-                            elseif id == 6682163754 or id == 6688734313 then
-                                TP = CFrame.new(241.43087768554688, 33.24515914916992, 450.21502685546875) -- b1c3p2
-                            elseif id == 6682164423 or id == 6688734395 then
-                                TP = CFrame.new(-654.1986694335938, 648.9832153320312, -1012.5780029296875) -- b1c3p3
-                            elseif id == 7068738088 or id == 7068951438 then
-                                TP = CFrame.new(2528.98681640625, 8.164044380187988, -1184.1917724609375) -- twt1
-                            elseif id == 7068951914 then
-                                TP = CFrame.new(-444.71759033203125, 20.493558883666992, -1453.7325439453125) -- twt2
-                            elseif id == 7068740106 or id == 7068952294 then
-                                TP = CFrame.new(2690.99169921875, 9.297460556030273, 649.3221435546875) -- twt3
-                            elseif id == 15996404472 or id == 15996407335 then
-                                TP = CFrame.new(3507.028564453125, 43.13663864135742, -1541.9735107421875) -- b1c1p1
-                            elseif id == 15996410294 or id == 15996411979 then
-                                TP = CFrame.new(64.74767303466797, 60.94379806518555, -1622.5250244140625) -- b1c2p1
-                            elseif id == 15996413469 then
-                                TP = CFrame.new(1055.5179443359375, 78.26939392089844, -351.45831298828125) -- b1c2p3
-                            elseif id == 15996414738 or id == 15996416081 then
-                                TP = CFrame.new(2411.138916015625, -23.03112030029297, 2267.944580078125) -- b1c3p1
-                            elseif id == 15996417416 then
-                                TP = CFrame.new(431.4171142578125, 17.476003646850586, -846.1138916015625) -- b1c3p3
-                            elseif id == 7265396387 or id == 7251865082 then
-                                TP = CFrame.new(85.20524597167969, -51.00001525878906, -1415.0792236328125) -- b1c4p1
-                            end
+                            Skip()
                         end
                     },
                     {
@@ -339,20 +407,6 @@ id == 15996413469 or id == 7265396387 or id == 7251865082 or id == then
                         {
                             Title = "Yes",
                             Callback = function()
-                                local TeleportService = game:GetService("TeleportService")
-                                local Players = game:GetService("Players")
-                                local LocalPlayer = Players.LocalPlayer
-                                local newPlaceId = 7618863566
-                        
-                                local function teleportToNewPlace()
-                                    local success, errorMessage = pcall(function()
-                                        TeleportService:Teleport(newPlaceId, LocalPlayer)
-                                    end)
-                        
-                                    if not success then
-                                        warn("Failed to teleport: " .. tostring(errorMessage))
-                                    end
-                                end
                                 teleportToNewPlace()
                             end
                         },

9f2d111c3586dfc722ab27e4e64918c685fdd92d
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-08 02:05:55 +0700

    Update MainTheMimic.lua

--- MainTheMimic.lua
+++ MainTheMimic.lua
@@ -153,8 +153,6 @@ function AutoOrbs()
 end
 
 function Autobtfs()
-    TP.HumanoidRootPart.CFrame = CFrame.new(1099.39794921875, 3.135153293609619, 75.5241928100586)
-    wait(1.5)
     for _, v in pairs(workspace.Butterflies:GetChildren()) do
         if v:IsA("BasePart") then
             -- Move the player's character to the position of the BasePart, with an offset
@@ -550,7 +548,9 @@ if id == 7265396805 or id == 7251866503 then
                     {
                         Title = "Yes",
                         Callback = function()
-                            _G.butterfly = true
+                                    TP.HumanoidRootPart.CFrame = CFrame.new(1099.39794921875, 3.135153293609619, 75.5241928100586)
+                                    wait(1.5)
+                            _G.buttlerfly = true
                             while _G.buttlefly do
                                 wait(0.1) -- Adjusted wait to avoid potential performance issues
                                 handleProximityPrompts()

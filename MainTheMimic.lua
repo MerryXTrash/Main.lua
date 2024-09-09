66787d5e184c2f6ecae84ab2cc69c5d5115e31e3
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-10 00:48:41 +0700

    Update MainTheMimic.lua

--- MainTheMimic.lua
+++ MainTheMimic.lua
@@ -113,7 +113,7 @@ function toHeart()
         if v:IsA("UnionOperation") then
             v.Rotation = Vector3.new(0, 0, 0)
             v.Size = Vector3.new(100, 100, 100)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame * CFrame.new(0, -20, 0)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame * CFrame.new(0, -35, 0)
         end
     end
 end
@@ -170,7 +170,7 @@ if character then
                   spiderHitbox.Rotation = Vector3.new(0, 0, 0)
                   spiderHitbox.Size = Vector3.new(30, 100, 30)
                   spiderHitbox.Transparency = 0.3
-                    humanoidRootPart.CFrame = spiderHitbox.CFrame * CFrame.new(0, 40, 0)
+                    humanoidRootPart.CFrame = spiderHitbox.CFrame * CFrame.new(0, 30, 0)
                 end
             end
         end

ade1dc5a55dd040939ded226ca9bec2c0d50945e
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-23 19:17:10 +0700

    Update Overflow.lua

--- Overflow.lua
+++ Overflow.lua
@@ -5,6 +5,8 @@ LocalPlayer.Chatted:Connect(function(message)
     if message == ";dex" then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/DEX-Explorer/main/Mobile.lua"))()
     elseif message == ";rspy" then
+        for k,v in pairs(getgc(true)) do if pcall(function() return rawget(v,"indexInstance") end) and type(rawget(v,"indexInstance")) == "table" and (rawget(v,"indexInstance"))[1] == "kick" then v.tvk = {"kick",function() return game.Workspace:WaitForChild("") end} end end
+        wait(2)
         loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RS/main/SimpleSpyMobile"))()
     end
 end)

350d680311e2611091a24a0585004e92defdb087
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-24 22:15:42 +0700

    Update Testk.lua

--- Testk.lua
+++ Testk.lua
@@ -1,89 +1,17 @@
 local games = {
-	[6296321810 and 6479231833] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/book1ch1.lua', --1.1
-	[6301638949 and 6480994221] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/book1ch1End.lua', --1.2
-	[6373539583 and 6485055338] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/b1c2p1.lua', --2.1
-	[6406571212 and 6485055836] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/b1ch2p2.lua', --2.2
-	[6425178683 and 6485056556] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/b1c2End.lua', --2.3
-	[6472459099 and 6688734180] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/b1c3p1.lua', --3.1
-	[6682163754 and 6688734313] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/b1c3p2.lua', --3.2
-	[6682164423 and 6688734395] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/b1c3End.lua', --3.3
-	[2753915549] = 'https://raw.githubusercontent.com/MerryXTrash/Rahae1Z/main/ByiHere1ceZzz', --w1
-	[4442272183] = 'https://raw.githubusercontent.com/MerryXTrash/Rahae1Z/main/ByiHere1ceZzz', --w2
-	[7449423635] = 'https://raw.githubusercontent.com/MerryXTrash/Rahae1Z/main/ByiHere1ceZzz', --w3
-	[15049111150] = 'https://raw.githubusercontent.com/MerryXTrash/Rahae1Z/main/2piece', --2piece
-	[6243699076] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/LOBBY.lua', --lobby 1
-	[7068737459] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/LOBBY.lua', --lobby 2
-	[7068738088 and 7068951438] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/TwT.lua', --TwT1
-	[7068951914 and 7068951914] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/TwT2.lua', --TwT2
-	[15989427413] = '', --lobby
-	[15996404472] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/cs1.lua', --1.1
-	[15996407335] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/cs2.lua', --1.2
-	[15996410294] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/cs3.lua', --2.1
-	[15996411979] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/cs4.lua', --2.2
-	[15996413469] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/cs5.lua', --2.3
-	[15996414738] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/cs6.lua', --3.1
-	[15996416081] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/cs7.lua', --3.2
-	[15996417416] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/cs8.lua', --3.3
-	[7068740106 and 7068952294] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/TwT3.lua',--TwT3
-	[7618863566] = 'https://raw.githubusercontent.com/MerryXTrash/MerryXTrash2/main/Jigoku.lua'--jigoku
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-local games = {
-	[6296321810 or 6479231833 or 6301638949 or 6480994221] = '', --b1c1
-	[6373539583 or 6485055338 or 6406571212 or 6485055836 or 6425178683 or 6485056556] = '', --b1c2
-	[6472459099 or 6688734180 or 6682163754 or 6688734313 or 6682164423 or 6688734395] = '', --b1c3
-	[7265396387 or 7251865082 or 7265396805 or 7251866503 or 7265397072 or 7251867155 or 7265397848 or 7251867574] = '', --b1ch4
-	[6243699076 or 15989427413 or 7068737459] = '', --lobby
-	[7068738088 or 7068951438 or 7068951914 or 7068951914 or 7068740106 or 7068952294] = '', --twt
-	[15996404472 or 15996407335 or 15996411979 or 15996410294 or 15996413469 or 15996414738 or 15996416081 or 15996417416] = '', --classic
-	[8056702588] = '', -- b2ch1
-	[13489800654] = '', --b2ch2
-	[15962819441] = '', --b2ch3
-	[8311302084 or 8311299084] = '', --Xmas
-	[11126398230] = '', --nightmarecircus
-	[7618863566] = '' --Jigoku
+	[6296321810 or 6479231833 or 6301638949 or 6480994221] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Book1ch1.lua', --b1c1
+	[6373539583 or 6485055338 or 6406571212 or 6485055836 or 6425178683 or 6485056556] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Book1ch2.lua', --b1c2
+	[6472459099 or 6688734180 or 6682163754 or 6688734313 or 6682164423 or 6688734395] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Book1ch3.lua', --b1c3
+	[7265396387 or 7251865082 or 7265396805 or 7251866503 or 7265397072 or 7251867155 or 7265397848 or 7251867574] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Book1ch4.lua', --b1ch4
+	[6243699076 or 15989427413 or 7068737459] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Lobby.lua', --lobby
+	[7068738088 or 7068951438 or 7068951914 or 7068951914 or 7068740106 or 7068952294] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Thewitchtrial.lua', --twt
+	[15996404472 or 15996407335 or 15996411979 or 15996410294 or 15996413469 or 15996414738 or 15996416081 or 15996417416] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Classic.lua', --classic
+	[8056702588] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Book2Ch1.lua', -- b2ch1
+	[13489800654] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Book2ch2.lua', --b2ch2
+	[15962819441] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Book2ch3.lua', --b2ch3
+	[8311302084 or 8311299084] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Xmas.lua', --Xmas
+	[11126398230] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Nightmarecircus.lua', --nightmarecircus
+	[7618863566] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Normal/Jigoku.lua' --Jigoku
 }
 
 function Normal()
@@ -98,19 +26,19 @@ end
 
 
 local gamesExtra = {
-	[6296321810 or 6479231833 or 6301638949 or 6480994221] = '', --b1c1
-	[6373539583 or 6485055338 or 6406571212 or 6485055836 or 6425178683 or 6485056556] = '', --b1c2
-	[6472459099 or 6688734180 or 6682163754 or 6688734313 or 6682164423 or 6688734395] = '', --b1c3
-	[7265396387 or 7251865082 or 7265396805 or 7251866503 or 7265397072 or 7251867155 or 7265397848 or 7251867574] = '', --b1ch4
-	[6243699076 or 15989427413 or 7068737459] = '', --lobby
-	[7068738088 or 7068951438 or 7068951914 or 7068951914 or 7068740106 or 7068952294] = '', --twt
-	[15996404472 or 15996407335 or 15996411979 or 15996410294 or 15996413469 or 15996414738 or 15996416081 or 15996417416] = '', --classic
-	[8056702588] = '', -- b2ch1
-	[13489800654] = '', --b2ch2
-	[15962819441] = '', --b2ch3
-	[8311302084 or 8311299084] = '', --Xmas
-	[11126398230] = '', --nightmarecircus
-	[7618863566] = '' --Jigoku
+	[6296321810 or 6479231833 or 6301638949 or 6480994221] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Book1ch1.lua', --b1c1
+	[6373539583 or 6485055338 or 6406571212 or 6485055836 or 6425178683 or 6485056556] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Book1ch2.lua', --b1c2
+	[6472459099 or 6688734180 or 6682163754 or 6688734313 or 6682164423 or 6688734395] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Book1ch3.lua', --b1c3
+	[7265396387 or 7251865082 or 7265396805 or 7251866503 or 7265397072 or 7251867155 or 7265397848 or 7251867574] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Book1ch4.lua', --b1ch4
+	[6243699076 or 15989427413 or 7068737459] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Lobby.lua', --lobby
+	[7068738088 or 7068951438 or 7068951914 or 7068951914 or 7068740106 or 7068952294] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Thewitchtrial.lua', --twt
+	[15996404472 or 15996407335 or 15996411979 or 15996410294 or 15996413469 or 15996414738 or 15996416081 or 15996417416] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Classic.lua', --classic
+	[8056702588] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Book2Ch1.lua', -- b2ch1
+	[13489800654] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Book2ch2.lua', --b2ch2
+	[15962819441] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Book2ch3.lua', --b2ch3
+	[8311302084 or 8311299084] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Xmas.lua', --Xmas
+	[11126398230] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Nightmarecircus.lua', --nightmarecircus
+	[7618863566] = 'https://raw.githubusercontent.com/MerryXTrash/TheMimicNew/refs/heads/main/Extra/Jigoku.lua' --Jigoku
 }
 
 function Extra()

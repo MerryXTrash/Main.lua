257f1a9c6077e2839db4b1c9445619803660e1f8
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-09-26 00:15:42 +0700

    Update Antilag.lua

--- Antilag.lua
+++ Antilag.lua
@@ -1,50 +1,37 @@
-        _G.Ignore = {}
-_G.Settings = {
-    Players = {
-        ["Ignore Me"] = true,
-        ["Ignore Others"] = true,
-        ["Ignore Tools"] = true
-    },
-    Meshes = {
-        NoMesh = false,
-        NoTexture = false,
-        Destroy = false
-    },
-    Images = {
-        Invisible = true,
-        Destroy = false
-    },
-    Explosions = {
-        Smaller = true,
-        Invisible = false, -- Not for PVP games
-        Destroy = false -- Not for PVP games
-    },
-    Particles = {
-        Invisible = true,
-        Destroy = false
-    },
-    TextLabels = {
-        LowerQuality = true,
-        Invisible = false,
-        Destroy = false
-    },
-    MeshParts = {
-        LowerQuality = true,
-        Invisible = false,
-        NoTexture = false,
-        NoMesh = false,
-        Destroy = false
-    },
-    Other = {
-        ["FPS Cap"] = 240, -- true to uncap
-        ["No Camera Effects"] = true,
-        ["No Clothes"] = true,
-        ["Low Water Graphics"] = true,
-        ["No Shadows"] = true,
-        ["Low Rendering"] = true,    
-        ["Low Quality Parts"] = true,
-        ["Low Quality Models"] = true,
-        ["Reset Materials"] = true,
-    }
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MerryXTrash/Main.lua/refs/heads/main/Sourceantilag"))()
+local a = tick()
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+wait(.1)
+sethiddenproperty(game.Lighting, "Technology", 2)
+sethiddenproperty(workspace:FindFirstChildOfClass("Terrain"), "Decoration", false)
+settings().Rendering.QualityLevel = 1
+setsimulationradius(0,0)
+game.Lighting.GlobalShadows = false
+game.Lighting.FogEnd = 9e9
+workspace:FindFirstChildOfClass("Terrain").Elasticity = 0
+for b, c in pairs(game:GetDescendants()) do
+    task.spawn(
+        function()
+            wait()
+            if c:IsA("DataModelMesh") then
+                sethiddenproperty(c, "LODX", Enum.LevelOfDetailSetting.Low)
+                sethiddenproperty(c, "LODY", Enum.LevelOfDetailSetting.Low)
+                c.CollisionFidelity = "Hull"
+            elseif c:IsA("UnionOperation") then
+                c.CollisionFidelity = "Hull"
+            elseif c:IsA("Model") then
+                sethiddenproperty(c, "LevelOfDetail", 1)
+            elseif c:IsA("BasePart") then
+                c.Reflectance = 0
+                c.CastShadow = false
+            end
+        end
+    )
+end
+for d, e in pairs(game.Lighting:GetChildren()) do
+    if e:IsA("PostEffect") then
+        e.Enabled = false
+    end
+end
+warn("Low graphics loaded! (" .. math.floor(tick() - a) .. "s)")
